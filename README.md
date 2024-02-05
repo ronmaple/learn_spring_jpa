496df76aea781035d0d57923a00ef9b2e7066c19
Ron Mapue <ronmapue@Rons-MacBook-Air.local>
2024-02-04 16:24:44 -0800

    spring jpa

---
+++ README.md
@@ -0,0 +1,4 @@
+Spring JPA
+
+- basically an ORM for Spring
+- similar to TypeORM which I've used before
---
+++ src/main/java/com/example/demo/Application.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student student = new Student(
+                    "John",
+                    "Doe",
+                    "example@example.com",
+                    21
+            );
+            studentRepository.save(student);
+        };
+    }
+}
